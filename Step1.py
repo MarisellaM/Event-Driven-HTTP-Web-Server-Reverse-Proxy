15a6ab936e4c95504d8eebff78ede94f14ce39f2
Marisella Mendoza <114708357+MarisellaM@users.noreply.github.com>
2026-09-08 21:18:27 -0500

    Create Step1.py
    Basic file start for learning sockets using a hardcoded message.

---
+++ Step1.py
@@ -0,0 +1,10 @@
+import socket 
+
+HOST = "localhost" 
+PORT = 8080
+
+## Hardcode the message to be sent to the server
+
+message = "Hello, Server!"
+## Send the message to the server
+HARDCODED_MESSAGE = message.encode('utf-8')
